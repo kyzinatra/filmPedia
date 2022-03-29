c6c016b1fbeeeb8f5e43b524f0c932b2d0b12d64
Danil <danilsolom@mail.ru>
2022-03-29 18:42:50 +0300

    Error component

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,33 @@
+import Alert from "@mui/material/Alert/Alert";
+import Snackbar from "@mui/material/Snackbar/Snackbar";
+import React, { FC, useEffect, useState } from "react";
+
+interface IError {
+  err: { [key: string]: any } | null;
+}
+
+const Error: FC<IError> = ({ err }) => {
+  const [isErrWindowOpen, setErrorWindowState] = useState(false);
+  useEffect(
+    () => (err ? setErrorWindowState(true) : setErrorWindowState(false)),
+    [err]
+  );
+  return (
+    <Snackbar
+      open={isErrWindowOpen}
+      autoHideDuration={4000}
+      onClose={(e, r) => r == "timeout" && setErrorWindowState(false)}
+    >
+      <Alert
+        onClose={() => setErrorWindowState(false)}
+        severity="error"
+        sx={{ width: "100%" }}
+      >
+        Received an Error {err?.status}
+        {err?.statusText ? ` (${err.statusText})` : ""}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default Error;
